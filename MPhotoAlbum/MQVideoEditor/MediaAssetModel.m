b9c0be068725b628d0825737641d960c54570217
lmq5294249 <476930814@qq.com>
2022-04-14 17:07:22 +0800

    Create MediaAssetModel.m

---
+++ MPhotoAlbum/MQVideoEditor/MediaAssetModel.m
@@ -0,0 +1,25 @@
+//
+//  MediaAssetModel.m
+//  抖音特效-音视频变速
+//
+//  Created by 林漫钦 on 2021/12/8.
+//  Copyright © 2021 mac. All rights reserved.
+//
+
+#import "MediaAssetModel.h"
+
+@implementation MediaAssetModel
+
+@end
+
+@implementation VideoAssetModel
+
+@end
+
+@implementation PhotoAssetModel
+
+@end
+
+@implementation MTransitionNode
+
+@end
