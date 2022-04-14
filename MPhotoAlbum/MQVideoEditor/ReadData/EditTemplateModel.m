3027493cc64b6211e9aa1820e202ddfb87494175
lmq5294249 <476930814@qq.com>
2022-04-14 17:06:44 +0800

    Create EditTemplateModel.m

---
+++ MPhotoAlbum/MQVideoEditor/ReadData/EditTemplateModel.m
@@ -0,0 +1,19 @@
+//
+//  EditTemplateModel.m
+//  GANBAN
+//
+//  Created by 林漫钦 on 2022/3/7.
+//
+
+#import "EditTemplateModel.h"
+
+
+@implementation EditTemplateModel
+
+@end
+
+
+@implementation EditUnitModel
+
+
+@end
