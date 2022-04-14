b8890dd1a635562655473071e011330c9976e0fb
lmq5294249 <476930814@qq.com>
2022-04-14 17:07:10 +0800

    Create MQVideoComposition.h

---
+++ MPhotoAlbum/MQVideoEditor/MQVideoComposition.h
@@ -0,0 +1,15 @@
+//
+//  MQVideoComposition.h
+//  GIFPlayDemo
+//
+//  Created by 林漫钦 on 2022/2/10.
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+
+@interface MQVideoComposition : NSObject<AVVideoCompositing>
+
+@end
+
