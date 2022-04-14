bcfcedacf5e69773e3ab3208781d6fe5f1d19069
lmq5294249 <476930814@qq.com>
2022-04-14 17:07:07 +0800

    Create MQVideoEditingViewController.h

---
+++ MPhotoAlbum/MQVideoEditor/MQVideoEditingViewController.h
@@ -0,0 +1,18 @@
+//
+//  MQVideoEditingViewController.h
+//  MPhotoAlbum
+//
+//  Created by 林漫钦 on 2022/4/12.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MQVideoEditingViewController : UIViewController
+
+@property (nonatomic, copy) NSDictionary *mediaDic;
+
+@end
+
+NS_ASSUME_NONNULL_END
