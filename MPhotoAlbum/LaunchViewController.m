46b1bcee1ebc1fd092ed43cc510d70864305d4be
lmq5294249 <476930814@qq.com>
2022-04-22 16:29:30 +0800

    Create LaunchViewController.m

---
+++ MPhotoAlbum/LaunchViewController.m
@@ -0,0 +1,31 @@
+//
+//  LaunchViewController.m
+//  MPhotoAlbum
+//
+//  Created by 林漫钦 on 2022/4/15.
+//
+
+#import "LaunchViewController.h"
+
+@interface LaunchViewController ()
+
+@end
+
+@implementation LaunchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
