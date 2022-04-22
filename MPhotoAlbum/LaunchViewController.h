cfec0c8bbba612528c9ac70aba904d912ee54ee6
lmq5294249 <476930814@qq.com>
2022-04-22 16:29:32 +0800

    Create LaunchViewController.h

---
+++ MPhotoAlbum/LaunchViewController.h
@@ -0,0 +1,16 @@
+//
+//  LaunchViewController.h
+//  MPhotoAlbum
+//
+//  Created by 林漫钦 on 2022/4/15.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LaunchViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
