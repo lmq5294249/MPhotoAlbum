7a069d5db9edaba5051f3f72740200aa32f718f1
lmq5294249 <476930814@qq.com>
2022-04-22 16:29:22 +0800

    Create UIView+Frame.h

---
+++ MPhotoAlbum/MQCategory/UIView+Frame.h
@@ -0,0 +1,29 @@
+//
+//  UIView+Frame.h
+//  VideoEdite
+//
+//  Created by Lin on 2017/11/2.
+//  Copyright © 2017年 Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define D_SCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
+#define D_SCREEN_HEIGHT ([[UIScreen mainScreen] bounds].size.height)
+
+@interface UIView (Frame)
+
+@property (nonatomic,assign) CGFloat x;
+@property (nonatomic,assign) CGFloat y;
+
+@property(nonatomic,assign) CGFloat top;
+@property(nonatomic,assign) CGFloat left;
+@property(nonatomic,assign) CGFloat bottom;
+@property(nonatomic,assign) CGFloat right;
+@property(nonatomic,assign) CGFloat centerX;
+@property(nonatomic,assign) CGFloat centerY;
+@property(nonatomic,assign) CGFloat width;
+@property(nonatomic,assign) CGFloat height;
+@property(nonatomic,assign) CGPoint origin;
+@property (nonatomic,assign) CGSize size;
+@end
