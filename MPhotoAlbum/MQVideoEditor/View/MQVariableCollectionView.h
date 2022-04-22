ed85b036c1f5acd1f568aa34f4823b02a7ab59a3
lmq5294249 <476930814@qq.com>
2022-04-22 16:28:51 +0800

    Create MQVariableCollectionView.h

---
+++ MPhotoAlbum/MQVideoEditor/View/MQVariableCollectionView.h
@@ -0,0 +1,28 @@
+//
+//  MQVariableCollectionView.h
+//  MPhotoAlbum
+//
+//  Created by 林漫钦 on 2022/4/22.
+//
+
+#import <UIKit/UIKit.h>
+#import "LocalAssetModel.h"
+#import "VideoEditorParamTypeHeader.h"
+
+@interface MQVariableCollectionView : UIView
+
+@property (nonatomic, strong) NSMutableDictionary *mediaDic;
+
+@property (nonatomic, strong) NSMutableArray <LocalAssetModel*>*mediaAssetArray;
+
+@property (nonatomic, assign) NSInteger fixedNum;
+
+@property (nonatomic, strong) UIView *fatherView;
+
+@property (nonatomic, copy) UpdateBlock updateBlock;
+
+-(void)reloadData;
+
+@end
+
+
