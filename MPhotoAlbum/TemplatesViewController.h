9a6a67dbdcdb6946eda299828c174ffa51417fe4
lmq5294249 <476930814@qq.com>
2022-11-01 09:26:29 +0800

    Create TemplatesViewController.h

---
+++ MPhotoAlbum/TemplatesViewController.h
@@ -0,0 +1,18 @@
+//
+//  TemplatesViewController.h
+//  MPhotoAlbum
+//
+//  Created by 林漫钦 on 2022/6/10.
+//
+
+#import <UIKit/UIKit.h>
+#import "VideoTemplateReader.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TemplatesViewController : UIViewController
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
