1062eb20520927a035ef29255797c87a3e06f0b6
lmq5294249 <476930814@qq.com>
2022-11-01 09:24:45 +0800

    Create MediaSelectionViewController.h

---
+++ MPhotoAlbum/MQVideoEditor/AlbumView/MediaSelectionViewController.h
@@ -0,0 +1,17 @@
+//
+//  MediaSelectionViewController.h
+//  MPhotoAlbum
+//
+//  Created by 林漫钦 on 2022/6/24.
+//  Copyright © 2022 Man. All rights reserved.
+//
+
+#import "HohemAlbumViewController.h"
+#import "EditTemplateModel.h"
+
+@interface MediaSelectionViewController : HohemAlbumViewController
+
+@property (nonatomic, strong) EditTemplateModel *templateModel;
+
+@end
+
